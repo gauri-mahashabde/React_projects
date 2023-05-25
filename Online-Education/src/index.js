915c49954e0a103b8fde227fd5d0f3e5e8088966
Gauri Mahashabde <gauri2interact@gmail.com>
2023-05-25 22:17:22 +0530

    components rendered

--- Online-Education/src/index.js
+++ Online-Education/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Components/App";
+import App from "./Components/HomePage/Home";
+import Home from "./Components/HomePage/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home/>
   </React.StrictMode>
 );
 
